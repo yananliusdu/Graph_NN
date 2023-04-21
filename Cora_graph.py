948c3208348499d5d0b170a169dd313feb91abc8
Yanan Liu <lyn.sdu@gmail.com>
2023-04-21 16:53:31 +0100

    updates

---
+++ Cora_graph.py
@@ -0,0 +1,147 @@
+# https://github.com/tomonori-masui/graph-neural-networks/blob/main/gnn_pyg_implementations.ipynb#enroll-beta
+
+from torch_geometric.datasets import Planetoid
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+import torch
+import networkx as nx
+import matplotlib.pyplot as plt
+import torch_geometric
+from torch_geometric.data import Data
+from torch_geometric.utils.convert import to_networkx
+from collections import Counter
+import random
+import numpy as np
+import torch_geometric.transforms as T
+import torch.nn as nn
+
+def make_deterministic(random_seed = 123):
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(random_seed)
+
+make_deterministic()
+
+
+dataset = Planetoid(root='Cora', name='Cora')
+
+def show_dataset_stats(dataset):
+    print(f"Number of graphs: {len(dataset)}")
+    print(f"Number of node classes: {dataset.num_classes}")
+    print(f"Number of node features: {dataset.num_node_features}")
+
+show_dataset_stats(dataset)
+
+def show_graph_stats(graph):
+    print(f"Number of nodes: {graph.x.shape[0]}")
+    print(f"Number of node features: {graph.x.shape[1]}")
+    print(f"Number of edges: {graph.edge_index.shape[1]}")
+
+graph = dataset[0]
+show_graph_stats(graph)
+
+print("Class Distribution:", sorted(Counter(graph.y.tolist()).items()))
+
+
+def convert_to_networkx(graph, n_sample=None):
+    g = to_networkx(graph, node_attrs=["x"])
+    y = graph.y.numpy()
+    if n_sample is not None:
+        sampled_nodes = random.sample(g.nodes, n_sample)
+        g = g.subgraph(sampled_nodes)
+        y = y[sampled_nodes]
+    return g, y
+
+def plot_graph(g, y):
+    fig, ax = plt.subplots(figsize=(9, 7))
+    nx.draw_spring(g, node_size=30, arrows=True, node_color=y, ax=ax)
+    plt.show()
+
+g, y = convert_to_networkx(graph, n_sample=1000)
+# plot_graph(g, y)
+
+split = T.RandomNodeSplit(num_val=0.1, num_test=0.2)
+graph = split(graph)  # resetting data split
+print(
+    f"train: {int(graph.train_mask.sum())}, ",
+    f"val: {int(graph.val_mask.sum())}, ",
+    f"test: {int(graph.test_mask.sum())}",
+)
+
+from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
+
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = GCNConv(dataset.num_node_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        output = self.conv2(x, edge_index)
+
+        return output
+
+def train_node_classifier(model, graph, optimizer, criterion, n_epochs=200):
+
+    for epoch in range(1, n_epochs + 1):
+        model.train()
+        optimizer.zero_grad()
+        out = model(graph)
+        loss = criterion(out[graph.train_mask], graph.y[graph.train_mask])
+        loss.backward()
+        optimizer.step()
+
+        pred = out.argmax(dim=1)
+        acc = eval_node_classifier(model, graph, graph.val_mask)
+
+        if epoch % 10 == 0:
+            print(f'Epoch: {epoch:03d}, Train Loss: {loss:.3f}, Val Acc: {acc:.3f}')
+
+    return model
+
+
+def eval_node_classifier(model, graph, mask):
+
+    model.eval()
+    pred = model(graph).argmax(dim=1)
+    correct = (pred[mask] == graph.y[mask]).sum()
+    acc = int(correct) / int(mask.sum())
+
+    return acc
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+graph = graph.to(device)
+
+gcn = GCN().to(device)
+optimizer_gcn = torch.optim.Adam(gcn.parameters(), lr=0.01, weight_decay=5e-4)
+criterion = nn.CrossEntropyLoss()
+
+gcn = train_node_classifier(gcn, graph, optimizer_gcn, criterion)
+
+
+test_acc = eval_node_classifier(gcn, graph, graph.test_mask)
+print(f'Test Acc: {test_acc:.3f}')
+
+
+def visualize_classification_result(model, graph):
+    model.eval()
+    pred = model(graph).argmax(dim=1)
+    # corrects = (pred[graph.test_mask] == graph.y[graph.test_mask]).numpy().astype(int)
+    corrects = (pred[graph.test_mask] == graph.y[graph.test_mask]).cpu().numpy().astype(int)
+    # test_index = np.arange(len(graph.x))[graph.test_mask.numpy()]
+    test_index = np.arange(len(graph.x))[graph.test_mask.cpu().numpy()]
+
+    g, y = convert_to_networkx(graph.cpu())
+    g_test = g.subgraph(test_index)
+
+    print("yellow node: correct \npurple node: wrong")
+    plot_graph(g_test, corrects)
+
+visualize_classification_result(gcn, graph)
